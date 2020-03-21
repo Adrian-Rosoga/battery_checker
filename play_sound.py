0c967a116be33ff371bf9398c4f3af3624b48fa4
Arvic68 <41381712+Arvic68@users.noreply.github.com>
2020-03-21 11:42:09 +0000

    Add files via upload

--- play_sound.py
+++ play_sound.py
@@ -1,4 +1,3 @@
 def play_bat_low():
     import playsound
-    playsound("C:/Users/Albert/Documents/Python/Battery_Check/test.mp3")
-play_bat_low()
+    playsound("C:/Users/%User%/Documents/Python/Battery_Check/test.mp3")
