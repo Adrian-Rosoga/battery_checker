4683781a144fc688ddb2ba0e5b018191a2fbe97d
Arvic68 <41381712+Arvic68@users.noreply.github.com>
2020-03-21 11:29:18 +0000

    Add files via upload

--- check_bat.py
+++ check_bat.py
@@ -1,10 +1,21 @@
-import psutil
+def check_bat():
+    import psutil
+    import time
+    import play_sound
 
-battery = psutil.sensors_battery()
-print(battery)
+    temp = 0
+    
+    while temp == 0: #every minute it checks the battery
+        
+        time.sleep(60)
+        battery = psutil.sensors_battery()
+        print(battery)
 
-remaining = battery.precent
-plugged = battery.power_plugged
+        remaining = battery.precent
+        plugged = battery.power_plugged
 
-if remaining <=20 and plugged == False:
-    print ("test")
+        if remaining <=20 and plugged == False:
+            play_sound.play_bat_low()
+
+def stop_check():
+    temp = 1
--- check_run.py
+++ check_run.py
@@ -3,9 +3,13 @@ num = int(file_open.read())
 file_open.close()
 
 file_open = open("running.txt","w")
+import check_bat as bat
 
 if num == 0:
     file_open.write("1") #if not running it start running
+    bat.check_bat()
+
 
 else:
     file_open.write("0") #shuts down the program
+    bat.stop_check()
