d96803e006fee133503c4f821c50f520e7ee6154
Arvic68 <41381712+Arvic68@users.noreply.github.com>
2020-03-21 11:34:24 +0000

    Add files via upload

--- check_run.py
+++ check_run.py
@@ -3,13 +3,13 @@ num = int(file_open.read())
 file_open.close()
 
 file_open = open("running.txt","w")
-import check_bat as bat
+from check_bat import check_batt, stop_check
 
 if num == 0:
     file_open.write("1") #if not running it start running
-    bat.check_bat()
+    check_batt()
 
 
 else:
     file_open.write("0") #shuts down the program
-    bat.stop_check()
+    stop_check()
